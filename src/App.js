4dd70f2991f970dd0eed77295a23771ecfed4af4
Michal <Michal.stachnik@my.liu.edu>
2017-09-06 16:07:01 -0400

    logic done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  state = {
+    alltime: [],
+    recent: [],
+    current: 'recent'
+  }
+
+  changeView(x){
+    this.setState({current: x})
+  }
+
+  componentDidMount(){
+    axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+    .then((response) => {
+      this.setState({
+        recent: response.data
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+    axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+    .then((response) => {
+      this.setState({
+        alltime: response.data
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
   render() {
+    let data = this.state[this.state.current]
+    console.log(data)
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <button onClick={() => this.changeView('recent')}>Recent</button>
+        <button onClick={() => this.changeView('alltime')}>All Time</button>
+        <h1>Displaying {this.state.current}</h1>
+        <ol>
+          {data.map((user, index) => (
+            <li key={index}>
+              <p>{user.username}</p>
+              <p>Alltime: {user.alltime}</p>
+              <p>Recent: {user.recent}</p>
+            </li>
+          ))}
+        </ol>
       </div>
     );
   }
 }
-
 export default App;
